3ef4ad04bab6512d2c4b6fdb80990879852a39fa
gabrielhilins <cmr.5344.gabriel@gmail.com>
2024-03-05 09:03:55 -0300

    get e set

--- Metodos/AgendaTelefonica.java
+++ Metodos/AgendaTelefonica.java
@@ -1,7 +1,7 @@
 public class AgendaTelefonica {
-    String nome;
-    String telefone;
-    String email;
+    private String nome;
+    private String telefone;
+    private String email;
 
     // Construtor
     AgendaTelefonica(String nome, String telefone, String email) {
@@ -40,6 +40,27 @@ public class AgendaTelefonica {
         } else {
             return false;
         }
+    }
+
+    // Getter
+    public String getNome() {
+        return this.nome;
+    }
+    public String getTelefone() {
+        return this.telefone;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    // Setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 }
